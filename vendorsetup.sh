25ce8eb7a3ae62e0f184c3f2eed972837220f139
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-11-22 08:21:30 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,21 +1,6 @@
-echo 'Start Cloning repos for LD7'
+#!/bin/bash
 
-echo 'Cloning Vendor Ims [1/4]'
-# Vendor Ims
-rm -rf vendor/mediatek/ims
-git clone --depth=1 https://github.com/Infinix-Devices-Series/vendor_mediatek_ims.git -b eleven vendor/mediatek/ims
-
-echo 'Cloning Vendor Trees [2/4]'
-# Vendor Blobs
-rm -rf vendor/tecno/LD7
-git clone https://github.com/Infinix-Devices-Series/android_vendor_tecno_LD7.git -b lineage-18.1 vendor/infinix/hot10
-
-echo 'Cloning Mediatek Opensource [3/4]'
-# Hardware Mediatek Opensource
-rm -rf hardware/mediatek/opensource
-git clone https://github.com/Infinix-Devices-Series/vendor_mediatek_opensource.git -b lineage-18.1 vendor/mediatek/opensource
-
-echo 'Cloning Mediatek Interfaces [4/4]'
-# Mediatek Interface
-rm -rf vendor/mediatek/interfaces
-git clone https://github.com/Infinix-Devices-Series/vendor_mediatek_interfaces.git -b lineage-18.1 vendor/mediatek/interfaces
+# Add lunch combo for your device
+add_lunch_combo lineage_LD7-user
+add_lunch_combo lineage_LD7-userdebug
+add_lunch_combo lineage_LD7-eng
